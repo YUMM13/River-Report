db480ae7760eebd6e561c529a6e8bcb66699f000
YUMM13 <blakestambaugh1@gmail.com>
2024-05-13 15:34:36 -0600

    Can now display river info on a table in the GUI

--- rr_webscraper.py
+++ rr_webscraper.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import customtkinter as ctk
+from tkinter import ttk
 
 # globals used for storing basin info
 colorado_basin = []
@@ -10,6 +11,8 @@ san_juan_basin = []
 great_basin = []
 salt_basin = []
 verde_basin = []
+
+dupe_check = False
 
 # method responsible for getting the river data
 def get_river_data():
@@ -32,6 +35,8 @@ def get_river_data():
     # iterate over all tables
     # with open('data.json', 'w') as file:
     for t in tables:
+        global dupe_check
+
         # iterate over each row in the table
         for r in t.find_all("tr"):
             for td in r.find_all("td"):
@@ -48,8 +53,15 @@ def get_river_data():
                 else:
                     forecast = td.text
 
+            
             # sort the data by basin
             if basin_name == "-Colorado Basin-" and river_name != "":
+                # deals with edge case because this river is mentioned twice
+                if river_name == "Colorado at Westwater":
+                    if not dupe_check:
+                        dupe_check = True
+                    else:
+                        break
                 colorado_basin.append((river_name, flow_rate, forecast))
             elif basin_name == "-Green River Basin-" and river_name != "":
                 green_river_basin.append((river_name, flow_rate, forecast))
@@ -62,7 +74,16 @@ def get_river_data():
             else:
                 verde_basin.append((river_name, flow_rate, forecast))
     
-    print(colorado_basin)
+    # sort arrays for consistency
+    colorado_basin.sort()
+    green_river_basin.sort()
+    san_juan_basin.sort()
+    great_basin.sort()
+    salt_basin.sort()
+    verde_basin.sort()
+
+    
+    #print(colorado_basin)
 
 
 # set color theme for gui
@@ -82,9 +103,31 @@ label.pack(pady=12, padx=10)
 
 get_river_data()
 
-# # Display data in the table
+# Display data in the table
 # for row, item in enumerate(colorado_basin, start=1):
 #     for col, value in enumerate(item):
 #         ctk.CTkLabel(frame, text=value).grid(row=row, column=col)
+
+# build table
+river_table = ttk.Treeview(frame)
+
+# define and format columns
+river_table['columns'] = ('River Name', 'Flow (cfs)', '24 Hour Forecast Trend')
+river_table.column('#0', width=0)
+river_table.column('River Name', anchor='w')
+river_table.column('Flow (cfs)', anchor='w')
+river_table.column('24 Hour Forecast Trend', anchor='w')
+
+# create headings
+river_table.heading('#0', text='label', anchor='w')
+river_table.heading('River Name', text='River Name', anchor='w')
+river_table.heading('Flow (cfs)', text='Flow (cfs)', anchor='w')
+river_table.heading('24 Hour Forecast Trend', text='24 Hour Forecast Trend', anchor='w')
+
+# add data
+for count, entry in enumerate(colorado_basin, start=1):
+    river_table.insert(parent='', index='end', iid=count, text='', values=(entry[0], entry[1], entry[2]))
+
+river_table.pack(pady=20)
 
 root.mainloop()
