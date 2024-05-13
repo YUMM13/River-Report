62974c0cbb10b959b781f17dd6efdb93aab9c8c1
YUMM13 <blakestambaugh1@gmail.com>
2024-05-13 17:47:27 -0600

    Logic is pretty much done. All I need at this point is to correctly sort the entries, but I need to make my own algorithm for it since I want to sort by river name.

--- rr_webscraper.py
+++ rr_webscraper.py
@@ -12,10 +12,15 @@ great_basin = []
 salt_basin = []
 verde_basin = []
 
-dupe_check = False
-
 # method responsible for getting the river data
 def get_river_data():
+    global colorado_basin
+    global green_river_basin
+    global san_juan_basin
+    global great_basin
+    global salt_basin
+    global verde_basin
+
     # make a GET response to the website
     rr_website = "https://www.cbrfc.noaa.gov/rec/rec.php"
     response = requests.get(rr_website)
@@ -27,17 +32,21 @@ def get_river_data():
     tables = soup.find_all("table")
 
     # organize the table into info, basin names, and data
-    basin_name = ""
-    river_name = ""
-    flow_rate = ""
-    forecast = ""
+
+    col = set()
+    green = set()
+    san_juan = set()
+    great = set()
+    salt = set()
+    verde = set()
 
     # iterate over all tables
-    # with open('data.json', 'w') as file:
     for t in tables:
-        global dupe_check
-
         # iterate over each row in the table
+        basin_name = ""
+        river_name = ""
+        flow_rate = ""
+        forecast = ""
         for r in t.find_all("tr"):
             for td in r.find_all("td"):
                 # get the river names
@@ -48,43 +57,62 @@ def get_river_data():
                     text = td.b.text.strip()
                     if text[0] == '-':
                         basin_name = text
+                        river_name = ""
+                        flow_rate = ""
+                        forecast = ""
                     else:
                         flow_rate = text
                 else:
                     forecast = td.text
 
-            
             # sort the data by basin
-            if basin_name == "-Colorado Basin-" and river_name != "":
-                # deals with edge case because this river is mentioned twice
-                if river_name == "Colorado at Westwater":
-                    if not dupe_check:
-                        dupe_check = True
-                    else:
-                        break
-                colorado_basin.append((river_name, flow_rate, forecast))
-            elif basin_name == "-Green River Basin-" and river_name != "":
-                green_river_basin.append((river_name, flow_rate, forecast))
-            elif basin_name == "-San Juan Basin-" and river_name != "":
-                san_juan_basin.append((river_name, flow_rate, forecast))
-            elif basin_name == "-Great Basin-" and river_name != "":
-                great_basin.append((river_name, flow_rate, forecast))
-            elif basin_name == "-Salt Basin-" and river_name != "":
-                salt_basin.append((river_name, flow_rate, forecast))
+            if basin_name == "-Colorado Basin-" and river_name != "" and river_name != None:
+                col.add((river_name, flow_rate, forecast))
+            elif basin_name == "-Green River Basin-" and river_name != "" and river_name != None:
+                green.add((river_name, flow_rate, forecast))
+            elif basin_name == "-San Juan Basin-" and river_name != "" and river_name != None:
+                san_juan.add((river_name, flow_rate, forecast))
+            elif basin_name == "-Great Basin-" and river_name != "" and river_name != None:
+                great.add((river_name, flow_rate, forecast))
+            elif basin_name == "-Salt Basin-" and river_name != "" and river_name != None:
+                salt.add((river_name, flow_rate, forecast))
             else:
-                verde_basin.append((river_name, flow_rate, forecast))
+                if river_name != None:
+                    verde.add((river_name, flow_rate, forecast))
     
-    # sort arrays for consistency
-    colorado_basin.sort()
-    green_river_basin.sort()
-    san_juan_basin.sort()
-    great_basin.sort()
-    salt_basin.sort()
-    verde_basin.sort()
+    # conver to arrays for ordering and list them
+    colorado_basin = list(col)
+    green_river_basin = list(green)
+    san_juan_basin = list(san_juan)
+    great_basin = list(great)
+    salt_basin = list(salt)
+    verde_basin = list(verde)
 
-    
-    #print(colorado_basin)
 
+def create_table(river_data, frame, ):
+    # build table
+    river_table = ttk.Treeview(frame)
+
+    # define and format columns
+    river_table['columns'] = ('River Name', 'Flow (cfs)', '24 Hour Forecast Trend')
+    river_table.column('#0', width=0, stretch='no')
+    river_table.column('River Name', anchor='w')
+    river_table.column('Flow (cfs)', anchor='w')
+    river_table.column('24 Hour Forecast Trend', anchor='w')
+
+    # create headings
+    river_table.heading('#0', text='', anchor='w')
+    river_table.heading('River Name', text='River Name', anchor='w')
+    river_table.heading('Flow (cfs)', text='Flow (cfs)', anchor='w')
+    river_table.heading('24 Hour Forecast Trend', text='24 Hour Forecast Trend', anchor='w')
+
+    # add data
+    for count, entry in enumerate(river_data, start=1):
+        river_table.insert(parent='', index='end', iid=count, text='', values=entry)
+
+    river_table.pack(pady=20)
+
+# ----------------------------------------------------- END OF HELPER METHODS ----------------------------------------------------------
 
 # set color theme for gui
 ctk.set_appearance_mode("light")
@@ -101,33 +129,10 @@ frame.pack(pady=20, padx=60, fill="both", expand=True)
 label = ctk.CTkLabel(master=frame, text="test")
 label.pack(pady=12, padx=10)
 
+# pull data from website
 get_river_data()
-
+print(colorado_basin)
 # Display data in the table
-# for row, item in enumerate(colorado_basin, start=1):
-#     for col, value in enumerate(item):
-#         ctk.CTkLabel(frame, text=value).grid(row=row, column=col)
-
-# build table
-river_table = ttk.Treeview(frame)
-
-# define and format columns
-river_table['columns'] = ('River Name', 'Flow (cfs)', '24 Hour Forecast Trend')
-river_table.column('#0', width=0)
-river_table.column('River Name', anchor='w')
-river_table.column('Flow (cfs)', anchor='w')
-river_table.column('24 Hour Forecast Trend', anchor='w')
-
-# create headings
-river_table.heading('#0', text='label', anchor='w')
-river_table.heading('River Name', text='River Name', anchor='w')
-river_table.heading('Flow (cfs)', text='Flow (cfs)', anchor='w')
-river_table.heading('24 Hour Forecast Trend', text='24 Hour Forecast Trend', anchor='w')
-
-# add data
-for count, entry in enumerate(colorado_basin, start=1):
-    river_table.insert(parent='', index='end', iid=count, text='', values=(entry[0], entry[1], entry[2]))
-
-river_table.pack(pady=20)
+create_table(colorado_basin, frame)
 
 root.mainloop()
